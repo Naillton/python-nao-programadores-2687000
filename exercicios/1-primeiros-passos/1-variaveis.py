a3467842c5e472bf0ed84efb70f1845854a442ad
Naillton <Nailton_junior@protonmail.com>
2025-06-18 07:03:13 -0300

    Ola mundo e declaracao de variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,14 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+first_name = "John"
+last_name = "Doe"
 
 # Declare variáveis de cada tipo numérico
-
+jhon_age = 30 # int
+height = 1.75  # float
+amelia = 20
 
 # Declare uma variável do tipo booleano
+isTrue = True
+isFalse = False
